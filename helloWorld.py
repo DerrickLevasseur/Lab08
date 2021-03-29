3cfc6f710ebaaa1194d5ccbac3300b0c5de3423b
DerrickLevasseur <70995489+DerrickLevasseur@users.noreply.github.com>
2021-03-28 23:29:25 -0400

    updated filed

--- helloWorld.py
+++ helloWorld.py
@@ -3,7 +3,12 @@ print ("Hello world")
 print ("Hello World, my name is Derrick and I love Python!")
 
 userName = input("Hello! Please enter your name!")
+
+userPassword = input("Hello! Please enter your facebook password!")
                 
 print("Hello", userName, "nice to meet you")
 
-
+if(userName.lower() == "derrick" and userPassword == "CyberSecurity!"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
