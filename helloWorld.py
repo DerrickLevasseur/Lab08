632fb9bc7041051a6e497d9bad22ea83c918e29c
DerrickLevasseur <70995489+DerrickLevasseur@users.noreply.github.com>
2021-03-28 23:11:41 -0400

    updated helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,9 @@
-Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/Derri/AppData/Local/Programs/Python/Python39/helloWorld.py =
-Hello! Please enter your name!Derrick Levasseur
-Hello Derrick Levasseur nice to meet you
->>> 
+print ("Hello world")
+
+print ("Hello World, my name is Derrick and I love Python!")
+
+userName = input("Hello! Please enter your name!")
+                
+print("Hello", userName, "nice to meet you")
+
+
